c72ed3b73e6417021f3dba5984d8362e7cb3f9f8
hendraaagil <hendraaagil@gmail.com>
2021-06-21 09:46:53 +0700

    Message Embed

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+const { Client, MessageEmbed } = require('discord.js');
+const bot = new Client();
 
 const token = 'ODU2MzUyODAwOTgzNDgyMzY4.YM_ysg.Md78cQdO_9Bzb6i-kdSw_uLUwNU';
 const PREFIX = '!';
+const version = '1.0.0';
 
 bot.on('ready', () => {
   console.log('Bot Sudah Online!');
@@ -25,11 +26,19 @@ bot.on('message', (message) => {
         if (args[1] === 'author') {
           message.channel.send('Hendra Agil @hendraaagil');
         } else if (args[1] === 'versi') {
-          message.channel.send('Versi 1.0.0');
+          message.channel.send('Versi : ' + version);
         } else {
           message.channel.send('Perintah tidak ditemukan!');
         }
       }
+      break;
+    case 'embed':
+      const embed = new MessageEmbed()
+        .setTitle('Informasi Bot')
+        .addField('Nama : ', 'Hendra Agil Syaputra')
+        .addField('Version : ', version)
+        .setFooter('Created by hendraaagil');
+      message.channel.send(embed);
       break;
   }
 });
